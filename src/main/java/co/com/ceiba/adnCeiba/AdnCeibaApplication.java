b7e373b479b58a0e93d1cfa8a074adb0ea0b6904
jefry.londono <jefry.londono@JEFRY-LONDONO.ceiba.local>
2019-02-02 21:43:14 -0500

    origin

---
+++ src/main/java/co/com/ceiba/adnCeiba/AdnCeibaApplication.java
@@ -0,0 +1,14 @@
+package co.com.ceiba.adnCeiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdnCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdnCeibaApplication.class, args);
+	}
+
+}
+
